b00d953c17377dbb9408fe7e7b1fcd662ad04fa0
Vladimir Chmyr <vladimir.chmyr@gmail.com>
2025-01-17 13:02:49 +0300

    'deploy'

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path';
 
 console.log(path.resolve(__dirname, 'src'))
 export default defineConfig({
-  base: '/vue-laslesvpn',
+  base: '/',
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
